64cd4b215c501972ccf5c10e77e1f99101c892a2
AntNowak <59796865+AntNowak@users.noreply.github.com>
2021-02-16 16:12:21 +0000

    mask train generator working

--- AttendanceMonitor/mask_train.py
+++ AttendanceMonitor/mask_train.py
@@ -5,62 +5,47 @@ import ImageUtils
 import numpy as np
 from os import path, mkdir, listdir
 
+#https://github.com/X-zhangyang/Real-World-Masked-Face-Dataset - masked dataset source
 class MaskTrain:
-
-    def __init__ (self):
-        self.cam = cv2.VideoCapture(0)
-
-    def __del__(self):
-        self.cam.release()
-
-    def create_student_dir(self, student_id):
-        user_data_dir = "user_data"
-        if (not path.exists(user_data_dir)):
-            mkdir(user_data_dir)
-        user_dir = user_data_dir + "/" + student_id
-        if (not path.exists(user_dir)):
-            mkdir(user_dir)
-        return user_dir
-
     def run_training(self):
-        user_data_dir = "user_data"
-        #finding user image directories
-        user_dirs = listdir(user_data_dir)
+        mask_data_dir = "mask_data"
+        masked_folder = "masked"
+        unmasked_folder = "unmasked"
+
+        #finding mask dataset directories
+        mask_dirs = listdir(mask_data_dir + "/" + masked_folder)
+        unmask_dirs = listdir(mask_data_dir + "/" + unmasked_folder)
         images = []
         ids = []
-        #interate the student IDs
-        for u in user_dirs:
-            user_files = listdir(user_data_dir + "/" + u)
-            #interating student images for IDs
+
+        #interate the masked data set
+        for u in mask_dirs:
+            user_files = listdir(mask_data_dir + "/" + masked_folder + "/" + u)
             for f in user_files:
-                image = cv2.imread(user_data_dir + "/" + u + "/" + f)
+                print(mask_data_dir + "/" + masked_folder + "/" + u + "/" + f)
+                if('┼' in f):
+                    continue
+                image = cv2.imread(mask_data_dir + "/" + masked_folder + "/" + u + "/" + f)
                 image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-                ids.append(int(u))
+                ids.append(1)
                 images.append(image)
+
+        #interate the unmasked data set
+        for u in unmask_dirs:
+            print(mask_data_dir + "/" + unmasked_folder + "/" + u)
+            user_files = listdir(mask_data_dir + "/" + unmasked_folder + "/" + u)
+            for f in user_files:
+                print(mask_data_dir + "/" + unmasked_folder + "/" + u + "/" + f)
+                image = cv2.imread(mask_data_dir + "/" + unmasked_folder + "/" + u + "/" + f)
+                image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+                ids.append(0)
+                images.append(image)
+
         recogniser = cv2.face.LBPHFaceRecognizer_create()
         recogniser.train(images, np.array(ids))
         recogniser.write("mask_train.yml")
 
-
-    def generate_training_set(self, student_id):
-        student_dir = self.create_student_dir(student_id)
-        number_of_images = 30
-        saved_images = 0
-        current_frame = 0
-
-        while saved_images < number_of_images:
-            ret, image = self.cam.read()
-            if(current_frame % 10 == 0):
-                if(not ret):
-                    return False
-                image, bounding_box = ImageUtils.crop_and_greyscale(image)
-                if(not image.size == 0):
-                    cv2.imwrite(student_dir + "/" + str(random.randint(0,50000))+ ".jpg", image)
-                    saved_images += 1
-            current_frame += 1
-
-r = Train()
-#r.generate_training_set("92")
+r = MaskTrain()
 r.run_training()
 
 
