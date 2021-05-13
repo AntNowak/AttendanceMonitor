9f7eea7bb564c7b83a6ffc2c004cea013c688c62
AntNowak <59796865+AntNowak@users.noreply.github.com>
2021-05-13 11:06:32 +0100

    small fix for crash

--- AttendanceMonitor/capture.py
+++ AttendanceMonitor/capture.py
@@ -11,12 +11,12 @@ class Sampler:
 
     def __del__(self):
         self.cam.release()
-
+    #creating temp directories for dataset
     def create_dirs(self):
         os.mkdir("temp_data")
         os.mkdir("temp_data/masked")
         os.mkdir("temp_data/unmasked")
-
+    #saving images taken 
     def save_image(self, im, masked = False):
         number = random.randint(0, 100000)
         if(masked):
@@ -25,22 +25,24 @@ class Sampler:
             cv2.imwrite("temp_data/unmasked/" + str(number) + ".jpg", im)
         return
 
+    #detecting face then displaying box around detected face
     def get_image(self):
         ret, image = self.cam.read()
         if(not ret):
             print("FAILED TO GET IMAGE")
-            image_color, bounding_box = image_utils.crop_colour(image)
-            if(not image_color.size == 0):
-                rsize_image = cv2.resize(image_color, (200, 200))
-                (x, y, w, h) = bounding_box
-                cv2.rectangle(image, (x, y), (x + w, y + h), (255, 0, 0), 2)
-                return 1, rsize_image, image
+        image_color, bounding_box = image_utils.crop_colour(image)
+        if(not image_color.size == 0):
+            rsize_image = cv2.resize(image_color, (200, 200))
+            (x, y, w, h) = bounding_box
+            cv2.rectangle(image, (x, y), (x + w, y + h), (255, 0, 0), 2)
+            return 1, rsize_image, image
         else:
             return -1, None, image
 
 r = Sampler()
 r.create_dirs()
 cv2.namedWindow('frame', cv2.WINDOW_NORMAL)
+#controls to take images after detecting face (only when box (detected face) is present)
 while True:
     success, r_image, i = r.get_image()
     cv2.imshow('video', i)
@@ -53,5 +55,6 @@ while True:
     elif k == 98 and success == 1: # press 'B' to save masked
         print("Saving Masked Image")
         r.save_image(r_image, True)
+    #no face detected whilst pressing A or B (no box around face) - nothing saved
     elif k == 97 or k == 98:
         print("Failed To Find Face")
